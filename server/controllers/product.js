d245510cabb84b8a99b8c7b0ad310a8c01e21dd8
Christian <citypowah93@gmail.com>
2022-01-06 18:58:06 +0100

    Rest API 1.0

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -6,37 +6,114 @@ function escapeRegex(text) {
 
 const getAllProducts = async (req, res) =>{
     try{
-        const {page = 1, perPage = 10, sort = {price:0,name:0}} = req.query;
-        const options = {
-            page: parseInt(page, 10),
-            limit: parseInt(perPage, 10),
-            sort: sort,
-            populate: {path: "id_provider"}
-        }
-        const data = await Product.paginate({},options)
-        res.status(200).json(data)
+        if (req.query.name) {
+            //http://localhost:3000/search?search=woxter&perPage=5&page=1&name=1
+            const {page = 1, perPage = 10, name} = req.query;
+            const options = {
+                page: parseInt(page, 10),
+                limit: parseInt(perPage, 10),
+                sort:  {name}
+            }
+            
+            const data = await Product.paginate({}, options);
+            res.status(200).json(data)}
+            else if (req.query.rating) {
+                //http://localhost:3000/search?search=woxter&perPage=5&page=1&rating=1
+                const {page = 1, perPage = 10, rating} = req.query;
+                const options = {
+                    page: parseInt(page, 10),
+                    limit: parseInt(perPage, 10),
+                    sort:  {rating}
+                }
+    
+                const data = await Product.paginate({}, options);
+                res.status(200).json(data)}
+
+            else if (req.query.price) {
+                //http://localhost:3000/search?search=woxter&perPage=5&page=1&price=1
+                const {page = 1, perPage = 10, price} = req.query;
+                const options = {
+                    page: parseInt(page, 10),
+                    limit: parseInt(perPage, 10),
+                    sort:  {price}
+                }      
+                const data = await Product.paginate({}, options);
+                res.status(200).json(data)}
+
+            else{
+                //http://localhost:3000/search?search=woxter&perPage=5&page=1&name=1
+                const {page = 1, perPage = 10} = req.query;
+                const options = {
+                    page: parseInt(page, 10),
+                    limit: parseInt(perPage, 10),
+                }    
+                const data = await Product.paginate({}, options);
+                res.status(200).json(data)
+            }
+
     } catch (err){
         res.status(400).json({'error':err})
     }
 }
 
 const searchProduct = async (req,res) =>{
-    try{        
-        if (req.query.search) {
-            const {page = 1, perPage = 10, sort = {price:0,name:0}} = req.query;
-
+    try{   
+        
+        // await Product.updateMany(
+        //     { 'price' : { $type: 2 } },
+        //     [{ $set: { 'price': { $toDouble: "$price" } } }]
+        // )
+        if (req.query.name) {
+            //http://localhost:3000/search?search=woxter&perPage=5&page=1&name=1
+            const {page = 1, perPage = 10, name} = req.query;
             const options = {
                 page: parseInt(page, 10),
                 limit: parseInt(perPage, 10),
-                sort: sort
+                sort:  {name}
             }
-
+            
             const regex = new RegExp(escapeRegex(req.query.search), 'gi');
             const data = await Product.paginate({ $or: [{name:regex}, {provider: regex}] }, options);
             res.status(200).json(data)}
+            else if (req.query.rating) {
+                //http://localhost:3000/search?search=woxter&perPage=5&page=1&rating=1
+                const {page = 1, perPage = 10, rating} = req.query;
+                const options = {
+                    page: parseInt(page, 10),
+                    limit: parseInt(perPage, 10),
+                    sort:  {rating}
+                }
+    
+                const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+                const data = await Product.paginate({ $or: [{name:regex}, {provider: regex}] }, options);
+                res.status(200).json(data)}
 
-    } catch (err){
-        res.status(400).json({'error':err})
+            else if (req.query.price) {
+                //http://localhost:3000/search?search=woxter&perPage=5&page=1&price=1
+                const {page = 1, perPage = 10, price} = req.query;
+                const options = {
+                    page: parseInt(page, 10),
+                    limit: parseInt(perPage, 10),
+                    sort:  {price}
+                }      
+                const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+                const data = await Product.paginate({ $or: [{name:regex}, {provider: regex}] }, options);
+                res.status(200).json(data)}
+
+            else{
+                //http://localhost:3000/search?search=woxter&perPage=5&page=1&name=1
+                const {page = 1, perPage = 10} = req.query;
+                const options = {
+                    page: parseInt(page, 10),
+                    limit: parseInt(perPage, 10),
+                }    
+                const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+                const data = await Product.paginate({ $or: [{name:regex}, {provider: regex}] }, options);
+                res.status(200).json(data)
+            }
+
+    } catch (error){
+        console.error(error.stack)
     }
 }
 
